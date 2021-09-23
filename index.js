80663e5d8019e49bbc30ac3aad16baf4b17088c8
raihanr1 <raihanrobbani123@gmail.com>
2021-09-23 17:51:01 +0700

    codecalculateLoan

---
+++ index.js
@@ -0,0 +1,14 @@
+function calculateLoan(params) {
+    let amount = document.querySelector('#amount').value
+    let month = document.querySelector('input[name=month]:checked').value
+    let monthlyInterest = amount * (5/100/month)
+    let paymentperbulan = ((amount/month) + monthlyInterest)
+    if (amount < 1000000) {
+        document.getElementById('payment').textContent = 'Amount minimum is Rp.1000.000'
+    } else {
+
+        document.getElementById('payment').textContent = paymentperbulan
+
+    }
+}
+
